a27cc8aa97073a843b8c48518f9cbb97ad1f586e
simbavoyages <kigali@simbavoyages.com>
2025-07-21 10:46:38 +0200

    removed baggage:

--- public/js/main.js
+++ public/js/main.js
@@ -47,11 +47,11 @@ function resetFareAndBaggageInputs() {
     document.getElementById('currencySelect').value = 'USD';
 
     // Reset baggage selection to "None"
-    document.getElementById('baggageNone').checked = true;
-    
+    document.getElementById('baggageParticular').checked = true;
+
     // Also trigger a 'change' event on the radio button.
     // This is important to make sure any associated UI logic (like hiding/showing other inputs) runs.
-    document.getElementById('baggageNone').dispatchEvent(new Event('change'));
+    document.getElementById('baggageParticular').dispatchEvent(new Event('change'));
 
     // If a result is currently displayed, update it to remove the now-cleared fare summary
     if (lastPnrResult) {
